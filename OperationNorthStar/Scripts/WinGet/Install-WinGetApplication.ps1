2321bbf7e968f8a75a34aefe2d8f72589d94f94e
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-11-12 15:45:31 +0100

    first version for test

--- OperationNorthStar/Scripts/WinGet/Install-WinGetApplication.ps1
+++ OperationNorthStar/Scripts/WinGet/Install-WinGetApplication.ps1
@@ -1,48 +1,36 @@
-function Install-WinGetApplication {
-    [CmdletBinding(DefaultParameterSetName = 'Single')]
-    param
-    (
-        [parameter(Mandatory, ParameterSetName = 'Single')]
-        [ValidateNotNullOrEmpty()]
-        [string]$AppName,
+[CmdletBinding(DefaultParameterSetName = 'Single')]
+param
+(
+    [parameter(Mandatory, ParameterSetName = 'Single',Position=0)]
+    [string]$AppName,
 
-        [parameter(Mandatory, ParameterSetName = 'Single')]
-        [ValidateNotNullOrEmpty()]
-        [string]$AppVersion,
+    [parameter(Mandatory, ParameterSetName = 'Single',Position=1)]
+    [string]$AppVersion,
 
-        [parameter(Mandatory, ParameterSetName = 'Single')]
-        [ValidateNotNullOrEmpty()]
-        [string]$AppScope,
+    
+    [parameter(Mandatory, ParameterSetName = 'Single',Position=2)]
+    [string]$Source
+)
 
-        [parameter(Mandatory, ParameterSetName = 'Single')]
-        [ValidateNotNullOrEmpty()]
-        [switch]$Force,
-
-        [parameter(Mandatory, ParameterSetName = 'Multiple')]
-        [ValidateNotNullOrEmpty()]
-        [string]$InstallFile
-    )
-
-    Begin {
-        Write-Verbose "Start searching for hostpool $hostpoolName"
-        AuthenticationCheck
-        $token = GetAuthToken -resource $script:AzureApiUrl
-        $apiVersion = "?api-version=2019-12-10-preview"
-        switch ($PsCmdlet.ParameterSetName) {
-            Single {
-                $installParameters = @{
-                    "--name" = $AppName
-                    "--version" = $AppVersion
-                    "--scope" = $AppScope
-                }
-            }
-            Multiple {
-
+Begin {
+    Write-Verbose "Start searching for hostpool $hostpoolName"
+    switch ($PsCmdlet.ParameterSetName) {
+        Single {
+            $installParameters = @{
+                "--name"    = $AppName
+                "--version" = $AppVersion
+                "--source" = $Source
             }
         }
+        Multiple {
+
+        }
     }
-    Process {
-           winget install @installParameters
-    }
+}
+Process {
+    $arguments = ($installParameters.GetEnumerator() | Sort-Object Name | ForEach-Object { "$($_.Name) $($_.Value)" }) -join " "
+    $argString = $arguments.ToString()
+    Write-Warning "Winget install $argString"
+    Invoke-Expression -Command "winget install $($arguments.ToString())"
 }
 
