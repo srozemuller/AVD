3452a3444cb99bcc65aa72dc7c3d9f3110973d4f
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-11-14 15:17:02 +0100

    newversion

--- OperationNorthStar/Scripts/WinGet/Install-WinGetApplication.ps1
+++ OperationNorthStar/Scripts/WinGet/Install-WinGetApplication.ps1
@@ -42,10 +42,10 @@ Process {
         Write-Warning "Folder $logfilePath does not exist, creating it."
         mkdir $logFilePath
     }
-    Write-Output "Winget $task $argString $switchArguments" | OutFile $logFile
+    Write-Output "Winget $task $argString $switchArguments" | Out-File $logFile
     $arguments = ($installParameters.GetEnumerator() | Sort-Object Name | ForEach-Object { "$($_.Name) $($_.Value)" }) -join " "
     $argString = $arguments.ToString()
     Write-Warning "Winget $task $argString $switchArguments"
-    Invoke-Expression -Command "cmd /c winget $task $($arguments.ToString()) $switchArguments"
+    Invoke-Expression -Command "winget $task $($arguments.ToString()) $switchArguments"
 }
 
