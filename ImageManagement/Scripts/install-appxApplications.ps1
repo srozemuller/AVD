f52299889cd0b7c67590f796cad49b2f6ba5f9a0
srozemuller@unica.nl <sander@rozemuller.com>
2021-11-29 09:18:28 +0100

    to msixbundle

--- ImageManagement/Scripts/install-appxApplications.ps1
+++ ImageManagement/Scripts/install-appxApplications.ps1
@@ -10,11 +10,11 @@ function Download-AppxPackage {
         #Get Urls to download
         Write-Host -ForegroundColor Yellow "Processing $Uri"
         $WebResponse = Invoke-WebRequest -UseBasicParsing -Method 'POST' -Uri 'https://store.rg-adguard.net/api/GetFiles' -Body "type=url&url=$Uri&ring=Retail" -ContentType 'application/x-www-form-urlencoded'
-        $LinksMatch = ($WebResponse.Links | where { $_ -like '*.appx*' } | where { $_ -like '*_neutral_*' -or $_ -like "*_" + $env:PROCESSOR_ARCHITECTURE.Replace("AMD", "X").Replace("IA", "X") + "_*" } | Select-String -Pattern '(?<=a href=").+(?=" r)').matches.value
-        $Files = ($WebResponse.Links | where { $_ -like '*.appx*' } | where { $_ -like '*_neutral_*' -or $_ -like "*_" + $env:PROCESSOR_ARCHITECTURE.Replace("AMD", "X").Replace("IA", "X") + "_*" } | where { $_ } | Select-String -Pattern '(?<=noreferrer">).+(?=</a>)').matches.value
+        $LinksMatch = ($WebResponse.Links | where { $_ -like '*.msixbundle*' } | where { $_ -like '*_neutral_*' -or $_ -like "*_" + $env:PROCESSOR_ARCHITECTURE.Replace("AMD", "X").Replace("IA", "X") + "_*" } | Select-String -Pattern '(?<=a href=").+(?=" r)').matches.value
+        $Files = ($WebResponse.Links | where { $_ -like '*.msixbundle*' } | where { $_ -like '*_neutral_*' -or $_ -like "*_" + $env:PROCESSOR_ARCHITECTURE.Replace("AMD", "X").Replace("IA", "X") + "_*" } | where { $_ } | Select-String -Pattern '(?<=noreferrer">).+(?=</a>)').matches.value
 
         for ($i = 0; $i -lt $LinksMatch.Count; $i++) {
-            $Array += , @($LinksMatch[$i], $Files[$i])
+            $Array += , @($LinksMatch, $Files)
         }
         $Array = $Array | sort-object @{Expression = { $_[1] }; Descending = $True }
         for ($i = 0; $i -lt $LinksMatch.Count; $i++) {
