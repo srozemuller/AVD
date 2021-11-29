8c201566fc23eb86999e2cd0c372880e3ca7b146
srozemuller@unica.nl <sander@rozemuller.com>
2021-11-29 15:33:24 +0100

    code optimzing for appx only

--- ImageManagement/Scripts/Install-AppxApplication.ps1
+++ ImageManagement/Scripts/Install-AppxApplication.ps1
@@ -1,14 +1,8 @@
-[CmdletBinding(DefaultParameterSetName = 'Manifest')]
+[CmdletBinding()]
 param
 (
-    [parameter(Mandatory, ParameterSetName = 'Single', Position = 1)]
-    [string]$AppName,
-
-    [parameter(ParameterSetName = 'Single', Position = 3)]
-    [string]$Source,
-
     [parameter()]
-    [string]$logFilePath = "C:\AppDeployment",
+    [string]$WorkingPath = "C:\AppDeployment",
 
     [parameter(ParameterSetName = 'Manifest', Position = 1)]
     [string]$AppInstallerLocation
@@ -17,67 +11,71 @@ param
 Begin {
     $date = get-date -format [yyyy-MM-dd_mm:ss]-
     $date = $date.ToString()
-    if (-not(Test-Path $logFilePath)) {
-        New-Item -ItemType Directory -Path $logFilePath
+    if (-not(Test-Path $WorkingPath)) {
+        New-Item -ItemType Directory -Path $WorkingPath
     }   
-    switch ($PsCmdlet.ParameterSetName) {
-        Manifest {
-            try {
-                if ($AppInstallerLocation.Contains('raw.githubusercontent.com')) {
-                    Write-Verbose "Raw GitHub content provided"
-                    $repoPath = ($AppInstallerLocation -split $($AppInstallerLocation -split "/")[5])[-1] 
-                }
-                else {
-                    Write-Verbose "Human readable GitHub content provided"
-                    $repoPath = ($AppInstallerLocation -split $($AppInstallerLocation -split "/")[6])[-1]
-                }
-                $arguments = 'https://api.github.com/repos/{3}/{4}/contents' -f ($AppInstallerLocation -split "/") + [string]$repoPath
-                $request = Invoke-WebRequest -Uri $($arguments) -UseBasicParsing:$true
-                $content = $request.Content | ConvertFrom-Json
-                $files = $content | Where-Object { $_.type -eq "file" } | Select-Object download_url, name
-                try {
-                    $files.download_url -match '.appinstaller$'
-                }
-                catch {
-                    throw "Location has no appinstaller file"
-                }
-                Write-Information "Downloading files"
-            }
-            catch {
-                Write-Error "Location not found!!"
-                break
-            }
+   
 
+    try {
+        if ($AppInstallerLocation.Contains('raw.githubusercontent.com')) {
+            Write-Verbose "Raw GitHub content provided"
+            $repoPath = ($AppInstallerLocation -split $($AppInstallerLocation -split "/")[5])[-1] 
         }
+        else {
+            Write-Verbose "Human readable GitHub content provided"
+            $repoPath = ($AppInstallerLocation -split $($AppInstallerLocation -split "/")[6])[-1]
+        }
+        $arguments = 'https://api.github.com/repos/{3}/{4}/contents' -f ($AppInstallerLocation -split "/") + [string]$repoPath
+        $request = Invoke-WebRequest -Uri $($arguments) -UseBasicParsing:$true
+        $content = $request.Content | ConvertFrom-Json
+        $files = $content | Where-Object { $_.type -eq "file" } | Select-Object download_url, name
+        try {
+            $files.download_url -match '.appinstaller$'
+            $appName = ($files | Where-Object { $_.name -match '.appinstaller$' }).Name.Split(".")[0] 
+        }
+        catch {
+            throw "Location has no appinstaller file"
+        }
+        Write-Information "Downloading files"
     }
-    if (-not(Test-Path (Join-Path -Path $logFilePath -ChildPath $AppName))) {
-        $AppWorkingPath = (New-Item -ItemType Directory -Path (Join-Path -Path $logFilePath -ChildPath 'MSIX')).FullName
+    catch {
+        Write-Error "Location not found!!"
+        break
+    }
+
+    if (-not(Test-Path (Join-Path -Path $WorkingPath -ChildPath $AppName))) {
+        $AppWorkingPath = (New-Item -ItemType Directory -Path (Join-Path -Path $WorkingPath -ChildPath 'MSIX')).FullName
     }
     else {
-        $AppWorkingPath = Join-Path -Path $logFilePath -ChildPath 'MSIX'
+        $AppWorkingPath = Join-Path -Path $WorkingPath -ChildPath 'MSIX'
     }
 }
 Process {
-    switch ($PsCmdlet.ParameterSetName) {
-        Manifest {
-            $appName = ($files | Where-Object {$_.name -match '.appinstaller$'}).Name.Split(".")[0] 
-            $templateFilePath = (New-Item -ItemType Directory -Path (Join-Path -Path $AppWorkingPath -ChildPath $appName)).FullName
-            $logFile = Join-Path -Path $AppWorkingPath -ChildPath 'install.log'
-            $files | ForEach-Object {
-                $requestParams = @{
-                    Uri             = $_.Download_url
-                    OutFile         = Join-Path -Path $templateFilePath -ChildPath $_.Name
-                    UseBasicParsing = $true
-                    Headers         = @{"Cache-Control" = "no-cache" }
-                }
-                Invoke-WebRequest @requestParams
-                Write-Output "Downloaded file $($_.Name)" | Out-File $logFile -Append
-            }
-            $appInstallerFile = Get-ChildItem -Path $templateFilePath | Where { $_.Name.Endswith('.appinstaller') } | Sort-Object | Select-Object -Last 1
+    $templateFilePath = (New-Item -ItemType Directory -Path (Join-Path -Path $AppWorkingPath -ChildPath $appName)).FullName
+    $logFile = Join-Path -Path $AppWorkingPath -ChildPath 'install.log'
+    $files | ForEach-Object {
+        $requestParams = @{
+            Uri             = $_.Download_url
+            OutFile         = Join-Path -Path $templateFilePath -ChildPath $_.Name
+            UseBasicParsing = $true
+            Headers         = @{"Cache-Control" = "no-cache" }
         }
-        default {
+        Invoke-WebRequest @requestParams
+        Write-Output "Downloaded file $($_.Name)" | Out-File $logFile -Append
+    }
+    $appInstallerFile = Get-ChildItem -Path $templateFilePath | Where { $_.Name.Endswith('.appinstaller') } | Sort-Object | Select-Object -Last 1
 
+    try {
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        if (-not (Get-Module -Name Appx -ListAvailable )) {
+            Install-Module -Name AppX -Force
+            Import-Module Appx -usewindowspowershell
+            Write-Output "Installing application based on $appInstallerFile" | Out-File $logFile -Append
+            $appInstallerFile | Add-AppPackage 
         }
     }
-    Add-AppPackage -AppInstallerFile $appInstallerFile
+    catch {
+        Throw "Install AppX module failed"
+    }
+ 
 }
