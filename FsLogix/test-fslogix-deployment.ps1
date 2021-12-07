5376e15eb8348da624786daba8a08e784f277f91
srozemuller <srozemuller@gmail.com>
2021-12-07 19:14:53 +0100

    code optimize

--- FsLogix/test-fslogix-deployment.ps1
+++ FsLogix/test-fslogix-deployment.ps1
@@ -1,9 +1,8 @@
-dsregcmd /RefreshPrt
-
 try {
     cmd /c "reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters /v CloudKerberosTicketRetrievalEnabled /t REG_DWORD /d 1"
     cmd /c "dsregcmd /RefreshPrt"
+    
 }   
 catch {
-    "Key added"
+    "Key not added"
 }
