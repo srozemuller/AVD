a0e71426675b7c2a68b7b430a93acb46d5935a6d
srozemuller@unica.nl <sander@rozemuller.com>
2021-12-10 16:03:07 +0100

    test with user/pass

--- FsLogix/test-kerberos-ticket.ps1
+++ FsLogix/test-kerberos-ticket.ps1
@@ -1,22 +1,22 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [ValidateNotNull()]
-    [System.Management.Automation.PSCredential]
-    [System.Management.Automation.Credential()]
-    $Credentials
+    [string]$userName,
+    [Parameter()]
+    [string]$password
 )
 try {
-    Write-Information "Getting Kerberos Ticket Granting Ticket from Micrsoft Online"
-    Start-Process  -Filepath powershell.exe -ArgumentList "klist purge" -Credential $credentials -NoNewWindow
-    Start-Process -Filepath powershell.exe -ArgumentList "klist get krbtgt" -Credential $credentials -NoNewWindow -WorkingDirectory '.' -RedirectStandardOutput "output.txt"
-    $output = Get-Content ".\output.txt"
+    $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+    Write-Host "Getting Kerberos Ticket Granting Ticket from Micrsoft Online" | Out-File "C:\Windows\output.txt" -Force
+    Start-Process -Filepath powershell.exe -ArgumentList "klist purge" -NoNewWindow -Credential $Credentials 
+    Start-Process -Filepath powershell.exe -ArgumentList "klist get krbtgt" -NoNewWindow -WorkingDirectory '.' -RedirectStandardOutput "C:\Windows\output.txt" -Credential $Credentials
+    $output = Get-Content "C:\Windows\output.txt"
     if ($output | Select-String -Pattern "Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM" -CaseSensitive -SimpleMatch) { 
         Write-Host "Got ticket from KERBEROS.MICROSOFTONLINE.COM" 
     }
     else {
         Throw "No ticket found from KERBEROS.MICROSOFTONLINE.COM"
-    }
+    }#>
 }   
 catch {
     Throw "Kerberos check failed, $_"
