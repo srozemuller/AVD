e81360d9d90fcbeada53c4f8a1939d89b8696eed
srozemuller <srozemuller@gmail.com>
2021-12-10 21:30:25 +0100

    test

--- FsLogix/test-kerberos-ticket.ps1
+++ FsLogix/test-kerberos-ticket.ps1
@@ -5,19 +5,22 @@ param (
     [Parameter()]
     [string]$password
 )
-try {
+#try {
     $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
-    Write-Host "Getting Kerberos Ticket Granting Ticket from Micrsoft Online" | Out-File "C:\Windows\output.txt" -Force
-    Start-Process -Filepath powershell.exe -ArgumentList "klist purge" -NoNewWindow -Credential $Credentials 
-    Start-Process -Filepath powershell.exe -ArgumentList "klist get krbtgt" -NoNewWindow -WorkingDirectory '.' -RedirectStandardOutput "C:\Windows\output.txt" -Credential $Credentials
-    $output = Get-Content "C:\Windows\output.txt"
-    if ($output | Select-String -Pattern "Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM" -CaseSensitive -SimpleMatch) { 
-        Write-Host "Got ticket from KERBEROS.MICROSOFTONLINE.COM" 
-    }
-    else {
-        Throw "No ticket found from KERBEROS.MICROSOFTONLINE.COM"
-    }#>
-}   
-catch {
-    Throw "Kerberos check failed, $_"
-}
+    #Write-Host "Getting Kerberos Ticket Granting Ticket from Micrsoft Online" | Out-File "C:\Windows\output.txt" -Force
+    #$credentials
+    cmd /c klist get krbtgt
+    #Start-Process -Filepath powershell.exe -verb RunAsUser -ArgumentList "echo hello" #-Credential $credentials
+    klist get krbtgt | Out-File c:\windows\temp\krbt.txt
+    #$output = Get-Content "C:\Windows\output.txt"
+    #if ($output | Select-String -Pattern "Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM" -CaseSensitive -SimpleMatch) { 
+    #    Write-Host "Got ticket from KERBEROS.MICROSOFTONLINE.COM" 
+    #}
+    #else {
+    #    Throw "No ticket found from KERBEROS.MICROSOFTONLINE.COM"
+    #}#>
+#}   
+#catch {
+#    Throw "Kerberos check failed, $_"
+#}
+
