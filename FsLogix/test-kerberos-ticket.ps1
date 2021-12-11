16ce309a3fd18657dcc4adeb8ad7e71cd34a535c
srozemuller <srozemuller@gmail.com>
2021-12-11 15:00:35 +0100

    tes

--- FsLogix/test-kerberos-ticket.ps1
+++ FsLogix/test-kerberos-ticket.ps1
@@ -9,9 +9,11 @@ param (
     #$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
     #Write-Host "Getting Kerberos Ticket Granting Ticket from Micrsoft Online" | Out-File "C:\Windows\output.txt" -Force
     #$credentials
-    cmd /c klist get krbtgt  | Out-File c:\windows\temp\krbt.txt
+    cmd.exe /c klist get krbtgt | Out-File c:\windows\temp\krbt.txt
     #Start-Process -Filepath powershell.exe -verb RunAsUser -ArgumentList "echo hello" #-Credential $credentials
-    klist get krbtgt | Out-File c:\windows\temp\krbt.txt -append
+    $output = klist get krbtgt
+    $output | Out-File c:\windows\temp\krbt.txt -append
+    Write-Host $output
     #$output = Get-Content "C:\Windows\output.txt"
     #if ($output | Select-String -Pattern "Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM" -CaseSensitive -SimpleMatch) { 
     #    Write-Host "Got ticket from KERBEROS.MICROSOFTONLINE.COM" 
