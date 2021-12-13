f0bef823797e13ae3cc2155bca32e20ed26a244b
srozemuller@unica.nl <sander@rozemuller.com>
2021-12-13 15:26:46 +0100

    test

--- FsLogix/test-kbrt.ps1
+++ FsLogix/test-kbrt.ps1
@@ -5,29 +5,8 @@ param (
     [Parameter()]
     [string]$password
 )
-#try {
-    $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
-    #Write-Host "Getting Kerberos Ticket Granting Ticket from Micrsoft Online" | Out-File "C:\Windows\output.txt" -Force
-    #$credentials
-    New-EventLog –LogName Application –Source "kbrt-test"
-    Write-EventLog –LogName Application –Source "kbrt-test" –EntryType Information –EventID 1 –Message "This is start."
-    Start-Process -FilePath 'powershell.exe' -Credential $credentials -ArgumentList ("klist get krbtgt | Out-File c:\test\kr.txt ") -WorkingDirectory 'C:\Windows\System32'
-    if ((Get-Content c:\test\kr.txt) -match 'Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM'){
-        Write-Host "Good" | Out-File C:\Test\output.txt
-        Write-EventLog –LogName Application –Source "kbrt-test" –EntryType Information –EventID 1 –Message "This is a test message."
-    }
-    else {
-        "Not good" | Out-File C:\Windows\Temp\output.txt
-    }
-    #$output = Get-Content "C:\Windows\output.txt"
-    #if ($output | Select-String -Pattern "Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM" -CaseSensitive -SimpleMatch) { 
-    #    Write-Host "Got ticket from KERBEROS.MICROSOFTONLINE.COM" 
-    #}
-    #else {
-    #    Throw "No ticket found from KERBEROS.MICROSOFTONLINE.COM"
-    #}#>
-#}   
-#catch {
-#    Throw "Kerberos check failed, $_"
-#}
 
+$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username, (ConvertTo-SecureString -String $password -AsPlainText -Force))
+#New-EventLog -LogName Application -Source "kbrt-test"
+Write-EventLog -LogName Application -Source "kbrt-test" -EntryType Information -EventID 1 -Message "This is start., $credentials"
+C:\Test\PsExec.exe -h -u $userName -p $password /accepteula cmd /c "klist get krbtgt > c:\test\out2.txt"
