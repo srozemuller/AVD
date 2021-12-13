07f122fadddbad55cb537ff8f49310600ef2784c
srozemuller@unica.nl <sander@rozemuller.com>
2021-12-13 09:50:56 +0100

    change file output

--- FsLogix/test-kbrt.ps1
+++ FsLogix/test-kbrt.ps1
@@ -9,19 +9,19 @@ param (
     #$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
     #Write-Host "Getting Kerberos Ticket Granting Ticket from Micrsoft Online" | Out-File "C:\Windows\output.txt" -Force
     #$credentials
-    New-EventLog –LogName Application –Source “kbrt-test”
-    Write-EventLog –LogName Application –Source “kbrt-test” –EntryType Information –EventID 1 –Message “This is start.”
+    New-EventLog –LogName Application –Source "kbrt-test"
+    Write-EventLog –LogName Application –Source "kbrt-test" –EntryType Information –EventID 1 –Message "This is start."
     if ((cmd.exe /c klist get krbtgt) -match 'Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM'){
-        Write-Host "Good" | Out-File C:\Windows\Temp\output.txt
-        Write-EventLog –LogName Application –Source “kbrt-test” –EntryType Information –EventID 1 –Message “This is a test message.”
+        Write-Host "Good" | Out-File C:\Test\output.txt
+        Write-EventLog –LogName Application –Source “kbrt-test” –EntryType Information –EventID 1 –Message "This is a test message."
     }
     else {
         "Not good" | Out-File C:\Windows\Temp\output.txt
     }
-    cmd.exe /c klist get krbtgt | Out-File c:\windows\temp\krbt.txt
+    cmd.exe /c klist get krbtgt | Out-File C:\Test\output.txt
     #Start-Process -Filepath powershell.exe -verb RunAsUser -ArgumentList "echo hello" #-Credential $credentials
     $output = klist get krbtgt
-    $output | Out-File c:\windows\temp\krbt.txt -append
+    $output | Out-File C:\Test\output.txt -append
     Write-Host $output
     #$output = Get-Content "C:\Windows\output.txt"
     #if ($output | Select-String -Pattern "Server: krbtgt/KERBEROS.MICROSOFTONLINE.COM @ KERBEROS.MICROSOFTONLINE.COM" -CaseSensitive -SimpleMatch) { 
